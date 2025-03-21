b4261b37b83b3ca03d1a8e77e51733cade72379b
Shubham Negi <160202370+Shubhii-A2Z@users.noreply.github.com>
2025-03-21 23:22:15 +0530

    Time: 21 ms (5.16%) | Memory: 27.5 MB (5.58%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -23,22 +23,13 @@ public:
         }
         return true;
     }
-
-    // bool f(vector<vector<char>>& board,int r,int c){
-    //     if(r==9 && c==9) return true;
-    //     if(!isSafe(board,r,c)){
-    //         return false;
-    //     }
-    //     return f(board,r+1,c+1);
-    // }
+    
     bool isValidSudoku(vector<vector<char>>& board) {
         for(int i=0;i<9;++i){
             for(int j=0;j<9;++j){
                 if(board[i][j]!='.'){
                     char num=board[i][j];
-                    // board[i][j]='.';
                     if(!isSafe(board,i,j)) return false;
-                    // board[i][j]=num;
                 }
             }
         }

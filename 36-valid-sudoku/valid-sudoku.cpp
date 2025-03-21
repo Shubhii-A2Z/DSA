52ecd9817822fb67411833379ad61098f4ba48e9
Shubham Negi <160202370+Shubhii-A2Z@users.noreply.github.com>
2025-03-21 23:21:52 +0530

    Time: 21 ms (5.16%) | Memory: 27.5 MB (5.58%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    bool isSafe(vector<vector<char>>& board,int r,int c){
+        unordered_set<char> down,right,cell;
+        for(int i=0;i<9;i++){
+            if(board[r][i]!='.' && right.count(board[r][i])) return false;
+            else right.insert(board[r][i]);
+        }
+
+        for(int i=0;i<9;i++){
+            if(board[i][c]!='.' && down.count(board[i][c])) return false;
+            else down.insert(board[i][c]);
+        }
+
+        int x=(r/3)*3;
+        int y=(c/3)*3;
+        for(int i=x;i<x+3;i++){
+            for(int j=y;j<y+3;j++){
+                if(board[i][j]!='.' && cell.count(board[i][j])) return false;
+                else cell.insert(board[i][j]);
+            }
+        }
+        return true;
+    }
+
+    // bool f(vector<vector<char>>& board,int r,int c){
+    //     if(r==9 && c==9) return true;
+    //     if(!isSafe(board,r,c)){
+    //         return false;
+    //     }
+    //     return f(board,r+1,c+1);
+    // }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<9;++i){
+            for(int j=0;j<9;++j){
+                if(board[i][j]!='.'){
+                    char num=board[i][j];
+                    // board[i][j]='.';
+                    if(!isSafe(board,i,j)) return false;
+                    // board[i][j]=num;
+                }
+            }
+        }
+        return true;
+    }
+};

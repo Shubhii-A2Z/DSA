9cfcb8d804d63c09ced497b5e81f40ea643b861a
Shubham Negi <160202370+Shubhii-A2Z@users.noreply.github.com>
2025-04-18 16:01:15 +0530

    Time: 52 ms (6.28%) | Memory: 111.1 MB (5.18%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        unordered_map<char,vector<int>> mp;
-        for(int i=0;i<colors.length();++i){
-            mp[colors[i]].push_back(neededTime[i]);
-        }
-
-        for(auto &[c,v]:mp){
-            sort(begin(v),end(v));
-        }
-
         int cst=0;
+        
         for(int i=1;i<colors.length();++i){
             if(colors[i]==colors[i-1]){
                 vector<int> c;

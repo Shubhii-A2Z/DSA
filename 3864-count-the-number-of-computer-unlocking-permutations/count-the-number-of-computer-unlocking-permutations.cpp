e035c9f8e3e06142b35ef256c4175544e687cecd
Shubham Negi <160202370+Shubhii-A2Z@users.noreply.github.com>
2025-06-10 17:32:20 +0530

    Time: 352 ms (5.01%) | Memory: 290.4 MB (5.23%) - LeetSync

---
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    #define MOD 1000000007
+
+    typedef long long ll;
+    vector<ll> fact;
+    vector<ll> invFact;
+
+    ll expo(ll a,ll b, ll m=MOD){ if(b==0) return 1%m; if(b%2==0){ int t=expo(a,b/2); return (1LL*t*t%m); }else{ int t=expo(a,(b-1)/2); t=(1LL*t*t)%m; return (1LL*a*t)%m; } }
+
+    void precomputefactorials(ll n){
+        fact.resize(n+1);
+        invFact.resize(n+1);
+
+        fact[0]=1;
+        for(int i=1;i<=n;i++) fact[i]=(fact[i-1]*i) % MOD; // O(n)
+        
+        invFact[n]=expo(fact[n],MOD-2);
+        for(int i=n-1;i>=0;i--) invFact[i]=(invFact[i+1]*(i+1)) % MOD; // O(n)
+    }
+
+    ll nCr(ll n,ll r){
+        if(r<0 || r>n) return 0;
+        return ((fact[n]*invFact[r]) % MOD*invFact[n-r]) % MOD;
+    }
+
+    int countPermutations(vector<int>& c) {
+        int mine=*min_element(begin(c),end(c));
+        if(mine!=c.front() || count(begin(c),end(c),c.front())>1) return 0;
+        
+        precomputefactorials(100005);
+        return fact[c.size()-1];
+    }
+};
